f7d6c499a21de780775def58f1f402a904e44dcf
Kauka Noorfathur <kaukanoer@gmail.com>
2022-12-25 14:42:56 +0800

    Moved isEmpty to separate file

---
+++ field-validate.js
@@ -0,0 +1,26 @@
+function isEmpty (payload) {
+    if (typeof payload === "object") {
+      if (Object.keys(payload).length) {
+        return false;
+      }
+      return true;
+    }
+    if (Array.isArray(payload)) {
+      if (payload.length) {
+        return false;
+      }
+      return true;
+    }
+    if (typeof payload === "string") {
+      if (payload) {
+        return false;
+      }
+      return true;
+    }
+    if (payload){
+      return false;    
+    }
+    return true;
+};
+
+module.exports = { isEmpty }
--- index.js
+++ index.js
@@ -1,28 +1,4 @@
 import { isEmail, maskEmail } from './email';
-
-const isEmpty = function (payload) {
-    if (typeof payload === "object") {
-      if (Object.keys(payload).length) {
-        return false;
-      }
-      return true;
-    }
-    if (Array.isArray(payload)) {
-      if (payload.length) {
-        return false;
-      }
-      return true;
-    }
-    if (typeof payload === "string") {
-      if (payload) {
-        return false;
-      }
-      return true;
-    }
-    if (payload){
-      return false;    
-    }
-    return true;
-};
+import { isEmpty } from './field-validate';
 
 module.exports = { isEmpty, isEmail, maskEmail }
