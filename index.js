5f800a188210aaf4ad19a74f14aadb7cfdbbd9c8
Achmad Eko <achmadeko003@gmail.com>
2022-12-28 13:45:16 +0700

    add isNumber

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { isEmail, maskEmail } = require("./lib/isEmail");
 const { stringValidator } = require("./lib/isString");
+const { numberValidator } = require("./lib/isNumber");
 const { isEmpty } = require("./lib/field-validate");
 
-module.exports = { isEmail, maskEmail, isEmpty, stringValidator };
+module.exports = { isEmail, maskEmail, isEmpty, stringValidator, numberValidator };
