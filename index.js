289ca8a77ba164cd8651333570d2c3b477c0b660
Achmad Eko <achmadeko003@gmail.com>
2022-12-29 18:44:33 +0700

    add object validate class

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const { isEmail, maskEmail } = require("./lib/isEmail");
+const { isEmpty } = require("./lib/field-validate");
 const { stringValidator } = require("./lib/isString");
 const { numberValidator } = require("./lib/isNumber");
-const { isEmpty } = require("./lib/field-validate");
+const { objectValidator } = require("./lib/isObject");
 
-module.exports = { isEmail, maskEmail, isEmpty, stringValidator, numberValidator };
+module.exports = {
+  isEmail,
+  maskEmail,
+  isEmpty,
+  isString: stringValidator,
+  isNumber: numberValidator,
+  isObject: objectValidator
+};
