788da109018e988af62fd1cd812c3dcba0918d4b
Achmad Eko <achmadeko003@gmail.com>
2023-01-06 00:11:45 +0700

    add validate array

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const { isEmpty } = require("./lib/field-validate");
 const { stringValidator } = require("./lib/isString");
 const { numberValidator } = require("./lib/isNumber");
 const { objectValidator } = require("./lib/isObject");
+const arrayValidator = require("./lib/isArray")
 
 module.exports = {
   isEmail,
   maskEmail,
   isEmpty,
+  isArray: arrayValidator,
   isString: stringValidator,
   isNumber: numberValidator,
   isObject: objectValidator
